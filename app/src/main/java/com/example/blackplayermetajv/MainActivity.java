b6f4e25c5dd72f17d183033c1b506af3c65a9538
Cage <gousaid67@gmail.com>
2022-04-07 19:49:23 +0200

    further AI work

--- app/src/main/java/com/example/blackplayermetajv/MainActivity.java
+++ app/src/main/java/com/example/blackplayermetajv/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.blackplayermetajv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import com.google.android.material.chip.Chip;
 import com.example.blackplayermetajv.NotificationListener;
 
 
@@ -24,21 +30,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MediaSessionManager mngr = (MediaSessionManager) this.getSystemService(Context.MEDIA_SESSION_SERVICE);
-        List<MediaController> mm_list = mngr.getActiveSessions(new ComponentName(this, NotificationListener.class));
+        List<MediaController> mm_list = GetMediaSessions(mngr);
         String str_list = Mc_to_Str(mm_list);
+        Spinner speeen = findViewById(R.id.mediasessionlist);
+        ArrayAdapter<Chip> inputlist = ArrayAdapter.createFromResource();
+        speeen.setOnItemSelectedListener();
 
 
         Log.i("", str_list);
     }
-    private String Mc_to_Str(List<MediaController> mm_list) {
+    public String Mc_to_Str(List<MediaController> mm_list) {
         StringBuilder new_str = new StringBuilder();
         for (MediaController i : mm_list)
         {
-            i.
+
             new_str.append(i.getPackageName() + "\n");
 
 
         }
         return new_str.toString();
+    }
+    public List<MediaController> GetMediaSessions(MediaSessionManager mngr)
+    {
+        return mngr.getActiveSessions(new ComponentName(this, NotificationListener.class));
     }
 }
